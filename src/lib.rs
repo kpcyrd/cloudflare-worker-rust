113164e8c44c779a66c577e6cf6929f1c91e87c5
kpcyrd <git@rxv.cc>
2024-02-19 21:58:15 +0100

    Add github actions CI

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
     console_debug!("Received request: {req:?}");
     Response::ok("Hello, World!")
 }
+
+#[cfg(test)]
+mod tests {
+    fn add(left: usize, right: usize) -> usize {
+        left + right
+    }
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
